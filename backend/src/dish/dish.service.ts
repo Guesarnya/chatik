971e5a08848674c2155feb31c629f2bd2c7bd48c
Guesarnya <mr.danil85@gmail.com>
2025-07-14 22:17:33 +0300

    Приложение готово к деплою

--- backend/src/dish/dish.service.ts
+++ backend/src/dish/dish.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@nestjs/common';
 import { HttpService } from '@nestjs/axios';
-import { AxiosResponse } from 'axios';
 import { firstValueFrom } from 'rxjs';
 
 @Injectable()
@@ -9,12 +8,11 @@ export class DishService {
 
   async findAll() {
     const userId = 475776712;
-
-    const response: AxiosResponse = await firstValueFrom(
-      this.httpService.get(`https://xn--d1arx6a.xn--p1ai/api/get_user?user_id=${userId}`)
+    
+    const { data } = await firstValueFrom(
+      this.httpService.get<any>(`https://xn--d1arx6a.xn--p1ai/api/get_user?user_id=${userId}`)
     );
 
-    const dishes = response.data; // это уже массив!
-    return dishes;
+    return data; // автоматически определит тип как any
   }
 }
