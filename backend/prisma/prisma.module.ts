9296b7e2993afe17a2900980392c6e40bdfca89d
Guesarnya <mr.danil85@gmail.com>
2025-07-14 22:37:27 +0300

    Приложение готово к деплою

--- backend/prisma/prisma.module.ts
+++ backend/prisma/prisma.module.ts
@@ -1,9 +1,9 @@
-import { Global, Module } from '@nestjs/common';
-import { PrismaService } from 'src/prisma.service';
+// import { Global, Module } from '@nestjs/common';
+// import { PrismaService } from 'src/prisma.service';
 
-@Global()
-@Module({
-  providers: [PrismaService],
-  exports: [PrismaService],
-})
-export class PrismaModule {}
+// @Global()
+// @Module({
+//   providers: [PrismaService],
+//   exports: [PrismaService],
+// })
+// export class PrismaModule {}
